e2c41d28832f8daebd6fd19b1d79cf36cb789016
jcfernandez <jcfernandez@theworkshop.com>
2022-06-21 14:54:51 +0200

    [oddOrEven] Step 5 - Should return even if the given param is 2

--- 01.oddeven/oddOrEven.js
+++ 01.oddeven/oddOrEven.js
@@ -1,6 +1,6 @@
 const oddOrEven = (number) => {
   const isOdd = (value) => value % 2 !== 0;
-  return isOdd(number) ? "odd" : number;
+  return isOdd(number) ? "odd" : "even";
 };
 
 module.exports = oddOrEven;
--- 01.oddeven/oddOrEven.test.js
+++ 01.oddeven/oddOrEven.test.js
@@ -1,12 +1,6 @@
 const oddOrEven = require("./oddOrEven");
 
 describe("when calling oddOrEven", () => {
-  test("should return the value that comes from params", () => {
-    const expected = 20;
-    const result = oddOrEven(expected);
-    expect(result).toBe(expected);
-  });
-
   test("should return odd then when the given param is 1", () => {
     const expected = "odd";
     const result = oddOrEven(1);
@@ -16,6 +10,12 @@ describe("when calling oddOrEven", () => {
   test("should return odd then when the given param is 3", () => {
     const expected = "odd";
     const result = oddOrEven(3);
+    expect(result).toBe(expected);
+  });
+
+  test("should return even then when the given param is 2", () => {
+    const expected = "even";
+    const result = oddOrEven(2);
     expect(result).toBe(expected);
   });
 });
