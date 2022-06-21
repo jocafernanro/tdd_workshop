bdce0f85310f5f580d23a6222a6d1a4c0fa62265
jcfernandez <jcfernandez@theworkshop.com>
2022-06-21 16:28:30 +0200

    [fizzbuzz] Step 10 - Optimal refactor

--- 02.fizzbuzz/fizzbuzz.js
+++ 02.fizzbuzz/fizzbuzz.js
@@ -2,16 +2,13 @@ const fizzbuzz = (number) => {
   if (typeof number !== "number" || number < 1)
     return "Error, the value must be greater than 0";
 
-  const isMultipleOf3 = (value) => value % 3 === 0;
-  const isMultipleOf5 = (value) => value % 5 === 0;
-  const isMultipleOf3And5 = (value) =>
-    isMultipleOf3(value) && isMultipleOf5(value);
+  const isMultipleOf = (divisor, value) => value % divisor === 0;
+  let output = "";
 
-  if (isMultipleOf3And5(number)) return "fizzbuzz";
-  if (isMultipleOf3(number)) return "fizz";
-  if (isMultipleOf5(number)) return "buzz";
+  if (isMultipleOf(3, number)) output += "fizz";
+  if (isMultipleOf(5, number)) output += "buzz";
 
-  return number;
+  return output;
 };
 
 module.exports = fizzbuzz;
